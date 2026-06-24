a5d3b11f849537280f906f8c021686e1969ab624
rajasreevijayapadmanaban-dotcom <rajasreevijayapadmanaban@gmail.com>
2026-06-24 11:53:50 +0530

    Create IOSCallDetector.js

---
+++ www/IOSCallDetector.js
@@ -0,0 +1,16 @@
+var exec = require("cordova/exec");
+
+module.exports = {
+
+    onCall: function(success, error) {
+
+        exec(
+            success,
+            error,
+            "IOSCallDetector",
+            "startListening",
+            []
+        );
+    }
+
+};
